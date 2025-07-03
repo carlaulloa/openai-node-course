9a711b93d2ff8f664d61295099b4581d7a8bf168
carlaulloa <carla.ulloa.lp@gmail.com>
2025-07-03 14:39:43 -0500

    feat: ortography chatgpt

--- nest-gpt/src/gpt/use-cases/ortography.usecase.ts
+++ nest-gpt/src/gpt/use-cases/ortography.usecase.ts
@@ -1,11 +1,31 @@
+import OpenAI from 'openai';
 interface Options {
   prompt: string;
-
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
-  const { prompt } = options
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
+  const { prompt } = options;
+
+  const response = await openai.responses.create({
+    model: 'gpt-4.1',
+    input: [
+      {
+        role: 'system',
+        content: 'Te serán proveídos textos con posibles errores ortográficos y gramáticales. Debes de responder en formato JSON. Tu tarea es corregirlos y retornar soluciones, también debes de dar un porcentaje de acierto por el usuario. Si no hay errores, debes de retornar un mensaje de felicitaciones.'
+      },
+      {
+        role: 'user',
+        content: `${prompt}`,
+      }
+    ],
+  });
+    console.log(response);
+  const jsonResponse = JSON.parse(response.output[0]['content'][0].text);
+
   return {
-    prompt: prompt
-  }
-}
+    output: jsonResponse
+  };
+};
